523ac1030df2adf386c7adc0c75aee3d1c519200
Zone.N <Zone.NiuZH@hotmail.com>
2023-05-03 22:11:29 +0800

    feat(nightvision): nightvision
    Signed-off-by: Zone.N <Zone.NiuZH@hotmail.com>

---
+++ data-updates.lua
@@ -0,0 +1,6 @@
+-- This file is a part of MRNIU/nzh_factorio_mod
+-- (https://github.com/MRNIU/nzh_factorio_mod).
+--
+-- data-updates.lua for MRNIU/nzh_factorio_mod.
+
+require("nightvision.data-updates")
---
+++ nightvision/data-updates.lua
@@ -0,0 +1,6 @@
+-- This file is a part of MRNIU/nzh_factorio_mod
+-- (https://github.com/MRNIU/nzh_factorio_mod).
+--
+-- data-updates.lua for MRNIU/nzh_factorio_mod.
+
+data.raw["night-vision-equipment"]["night-vision-equipment"].color_lookup = {{0.5, "__core__/graphics/color_luts/lut-sunset.png"}}
