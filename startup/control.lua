85e90034dde4a2b1e5138f6c4439875a1eeca6d5
Zone.N <zone.niuzh@hotmail.com>
2024-07-18 19:43:32 +0800

    1.3.0
    Signed-off-by: Zone.N <zone.niuzh@hotmail.com>

--- startup/control.lua
+++ startup/control.lua
@@ -14,33 +14,25 @@ local player_inventory_items = {
 	-- 三级组装机
 	{ name = "assembling-machine-3",    count = 50 },
 	-- 传送带
-	{ name = "transport-belt",          count = 1000 },
+	{ name = "transport-belt",          count = 200 },
 	-- 地下传送带
-	{ name = "underground-belt",        count = 200 },
+	{ name = "underground-belt",        count = 50 },
 	-- 分流器
-	{ name = "splitter",                count = 100 },
+	{ name = "splitter",                count = 50 },
 	-- 管道
-	{ name = "pipe",                    count = 1000 },
+	{ name = "pipe",                    count = 200 },
 	-- 地下管道
-	{ name = "pipe-to-ground",          count = 200 },
+	{ name = "pipe-to-ground",          count = 100 },
 	-- 机械臂
-	{ name = "inserter",                count = 200 },
+	{ name = "inserter",                count = 50 },
 	-- 加长机械臂
-	{ name = "long-handed-inserter",    count = 200 },
+	{ name = "long-handed-inserter",    count = 50 },
 	-- 快速机械臂
-	{ name = "fast-inserter",           count = 200 },
-	-- 实验室
-	{ name = "lab",                     count = 10 },
-	-- 炼油厂
-	{ name = "oil-refinery",            count = 10 },
-	-- 化工厂
-	{ name = "chemical-plant",          count = 10 },
+	{ name = "fast-inserter",           count = 50 },
 	-- 水泵
 	{ name = "offshore-pump",           count = 2 },
 	-- 油井
 	{ name = "pumpjack",                count = 10 },
-	-- 钢炉
-	{ name = "steel-furnace",           count = 50 },
 	-- 电力采矿机
 	{ name = "electric-mining-drill",   count = 50 },
 	-- 蜘蛛机甲遥控器
@@ -48,7 +40,7 @@ local player_inventory_items = {
 	-- 建设机器人
 	{ name = "construction-robot",      count = 100 },
 	-- 电线杆
-	{ name = "small-electric-pole",     count = 200 },
+	{ name = "small-electric-pole",     count = 50 },
 	{ name = "medium-electric-pole",    count = 50 },
 	{ name = "big-electric-pole",       count = 50 },
 	{ name = "substation",              count = 50 },
@@ -56,18 +48,119 @@ local player_inventory_items = {
 	{ name = "radar",                   count = 50 },
 	-- 贫铀穿甲弹
 	{ name = "uranium-rounds-magazine", count = 1000 },
+	-- 机枪炮塔
+	{ name = "gun-turret",              count = 50 },
+	-- 修理包
+	{ name = "repair-pack",             count = 100 },
+	-- 管道泵
+	{ name = "pump",                    count = 4 },
+	-- 机器人指令平台
+	{ name = "roboport",                count = 10 },
+}
+
+-- 物品箱 1 物品
+local chest1_items = {
+	-- 钢箱
+	{ name = "steel-chest",          count = 100 },
+	-- 一级组装机
+	{ name = "assembling-machine-1", count = 100 },
+	-- 二级组装机
+	{ name = "assembling-machine-2", count = 50 },
+	-- 三级组装机
+	{ name = "assembling-machine-3", count = 50 },
+	-- 传送带
+	{ name = "transport-belt",       count = 2400 },
+	-- 地下传送带
+	{ name = "underground-belt",     count = 300 },
+	-- 分流器
+	{ name = "splitter",             count = 100 },
+	-- 管道
+	{ name = "pipe",                 count = 200 },
+	-- 地下管道
+	{ name = "pipe-to-ground",       count = 200 },
+}
+
+-- 物品箱 2 物品
+local chest2_items = {
+	-- 热能机械臂
+	{ name = "burner-inserter",      count = 600 },
+	-- 机械臂
+	{ name = "inserter",             count = 600 },
+	-- 加长机械臂
+	{ name = "long-handed-inserter", count = 200 },
+	-- 快速机械臂
+	{ name = "fast-inserter",        count = 200 },
+	-- 电线杆
+	{ name = "small-electric-pole",  count = 400 },
+	{ name = "medium-electric-pole", count = 100 },
+	{ name = "big-electric-pole",    count = 50 },
+	{ name = "substation",           count = 50 },
+}
+
+-- 物品箱 3 物品
+local chest3_items = {
+	-- 实验室
+	{ name = "lab",                   count = 40 },
+	-- 炼油厂
+	{ name = "oil-refinery",          count = 20 },
+	-- 化工厂
+	{ name = "chemical-plant",        count = 20 },
+	-- 水泵
+	{ name = "offshore-pump",         count = 20 },
+	-- 油井
+	{ name = "pumpjack",              count = 20 },
+	-- 钢炉
+	{ name = "steel-furnace",         count = 400 },
+	-- 电力采矿机
+	{ name = "electric-mining-drill", count = 400 },
+}
+
+-- 物品箱 4 物品
+local chest4_items = {
+	-- 雷达
+	{ name = "radar",                   count = 100 },
+	-- 贫铀穿甲弹
+	{ name = "uranium-rounds-magazine", count = 2000 },
+	-- 机枪炮塔
+	{ name = "gun-turret",              count = 200 },
+	-- 锅炉
+	{ name = "boiler",                  count = 40 },
+	-- 蒸汽机
+	{ name = "steam-engine",            count = 80 },
+	-- 火焰炮塔
+	{ name = "flamethrower-turret",     count = 200 },
+	-- 石墙
+	{ name = "stone-wall",              count = 1000 },
+}
+
+-- 物品箱 5 物品
+local chest5_items = {
+	-- 石墙
+	{ name = "stone-wall",              count = 2000 },
+	-- 常量运算器
+	{ name = "constant-combinator", count = 200 },
+	-- 筛选机械臂
+	{ name = "filter-inserter",     count = 200 },
+	-- 集装机械臂
+	{ name = "stack-inserter",      count = 200 },
+	-- 修理包
+	{ name = "repair-pack",         count = 100 },
+	-- 储液罐
+	{ name = "storage-tank",        count = 50 },
+	-- 管道泵
+	{ name = "pump",                count = 50 },
+	-- 灯
+	{ name = "small-lamp",          count = 200 },
 }
 
 -- 能量装甲装备
 local power_armor_grid_items = {
 	-- 能量盾模块 MK2
-	{ name = "energy-shield-mk2-equipment",      count = 20 },
-	-- 外骨骼模块
-	{ name = "exoskeleton-equipment",            count = 20 },
+	{ name = "energy-shield-mk2-equipment",      count = 30 },
 	-- 聚变堆模块
 	{ name = "fusion-reactor-equipment",         count = 20 },
 	-- 激光防御模块
-	{ name = "personal-laser-defense-equipment", count = 20 },
+	{ name = "personal-laser-defense-equipment", count = 30 },
 	-- 机器人指令模块
 	{ name = "personal-roboport-mk2-equipment",  count = 18 },
 	-- 夜视模块
@@ -79,13 +172,11 @@ local power_armor_grid_items = {
 -- 蜘蛛机甲装备
 local spidertron_grid_items = {
 	-- 能量盾模块 MK2
-	{ name = "energy-shield-mk2-equipment",      count = 10 },
-	-- 外骨骼模块
-	{ name = "exoskeleton-equipment",            count = 20 },
+	{ name = "energy-shield-mk2-equipment",      count = 20 },
 	-- 聚变堆模块
 	{ name = "fusion-reactor-equipment",         count = 10 },
 	-- 激光防御模块
-	{ name = "personal-laser-defense-equipment", count = 10 },
+	{ name = "personal-laser-defense-equipment", count = 20 },
 	-- 机器人指令模块
 	{ name = "personal-roboport-mk2-equipment",  count = 8 },
 	-- 夜视模块
@@ -115,13 +206,13 @@ local spidertron_trunk_items = {
 	{ name = "explosive-rocket",     count = 800 },
 }
 
+local is_startup_inited = {}
+local is_start_box_inited = false
+
 local function main(player)
 	if player == nil then return end
-	if global.nzh_startup_inited == nil then
-		global.nzh_startup_inited = {}
-	end
 
-	if global.nzh_startup_inited[player.index] == true then
+	if is_startup_inited[player.index] == true then
 		return
 	end
 
@@ -167,7 +258,32 @@ local function main(player)
 	-- 设置驾驶员
 	spidertron.set_driver(player)
 
-	global.nzh_startup_inited[player.index] = true
+	-- 创建初始物品箱
+	if is_start_box_inited == false then
+		local logistic_chest_storage1 = player.surface.create_entity { name = "logistic-chest-storage", position = { x = player.position.x - 2, y = player.position.y + 4 }, force = player.force }
+		local logistic_chest_storage2 = player.surface.create_entity { name = "logistic-chest-storage", position = { x = player.position.x - 1, y = player.position.y + 4 }, force = player.force }
+		local logistic_chest_storage3 = player.surface.create_entity { name = "logistic-chest-storage", position = { x = player.position.x, y = player.position.y + 4 }, force = player.force }
+		local logistic_chest_storage4 = player.surface.create_entity { name = "logistic-chest-storage", position = { x = player.position.x + 1, y = player.position.y + 4 }, force = player.force }
+		local logistic_chest_storage5 = player.surface.create_entity { name = "logistic-chest-storage", position = { x = player.position.x + 2, y = player.position.y + 4 }, force = player.force }
+		for _, item in ipairs(chest1_items) do
+			logistic_chest_storage1.insert(item)
+		end
+		for _, item in ipairs(chest2_items) do
+			logistic_chest_storage2.insert(item)
+		end
+		for _, item in ipairs(chest3_items) do
+			logistic_chest_storage3.insert(item)
+		end
+		for _, item in ipairs(chest4_items) do
+			logistic_chest_storage4.insert(item)
+		end
+		for _, item in ipairs(chest5_items) do
+			logistic_chest_storage5.insert(item)
+		end
+		is_start_box_inited = true
+	end
+
+	is_startup_inited[player.index] = true
 end
 
 function startup_OnPlayerFirstJoinedGame(event)
